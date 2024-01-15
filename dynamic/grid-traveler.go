03dc7541b1364d0635704a27a14ac22bfac328e9
Carlos Salamanca <carlos@iamsitting.com>
2024-01-15 11:27:48 -0600

    tab grid

--- dynamic/grid-traveler.go
+++ dynamic/grid-traveler.go
@@ -11,6 +11,29 @@ import (
 	"strconv"
 )
 
+func gridTraverler3(x int, y int) int {
+	table := make([][]int, y+1)
+	for rowI := range table {
+		table[rowI] = make([]int, x+1)
+	}
+	table[0][0] = 0
+	table[1][1] = 1
+	fmt.Println("table:", table)
+	for j := 0; j <= y; j++ {
+		for i := 0; i <= x; i++ {
+			if i+1 <= x {
+				table[j][i+1] += table[j][i]
+			}
+
+			if j+1 <= y {
+				table[j+1][i] += table[j][i]
+			}
+		}
+	}
+
+	return table[y][x]
+}
+
 func gridTraveler(x int, y int) int {
 	if x == 0 || y == 0 {
 		return 0
@@ -42,6 +65,7 @@ func gridTraveler2(x int, y int, memo map[string]int) int {
 }
 
 func main() {
+	fmt.Println(gridTraverler3(2, 3))
 	fmt.Println(gridTraveler2(2, 3, map[string]int{}))
 	fmt.Println(gridTraveler2(3, 2, map[string]int{}))
 	fmt.Println(gridTraveler2(1, 1, map[string]int{}))
