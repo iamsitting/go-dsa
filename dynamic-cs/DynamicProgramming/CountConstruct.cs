fbf06e26f8ec83ab1b54e2ffe1953ec80390be98
Carlos Salamanca <carlos@iamsitting.com>
2024-01-15 09:56:27 -0600

    tab count construct

--- dynamic-cs/DynamicProgramming/CountConstruct.cs
+++ dynamic-cs/DynamicProgramming/CountConstruct.cs
@@ -2,19 +2,42 @@ namespace DynamicProgramming;
 
 public static class CountConstruct
 {
+    private static int TabSolve(string target, string[] wordBank)
+    {
+        var table = new int[target.Length + 1];
+        table[0] = 1; // there is 1 way to build an empty string
+        for (var i = 0; i <= target.Length; i++)
+        {
+            if (table[i] > 0)
+            {
+                foreach(var word in wordBank)
+                {
+                    if(i + word.Length <= target.Length)
+                    {
+                        var prefix = target.Substring(i, word.Length);
+                        if(prefix == word)
+                        {
+                            table[i + word.Length] += table[i];
+                        }
+                    }
+                }
+            }
+        }
+        return table[target.Length];
+    }
     private static int Solve(string target, string[] wordBank)
     {
         if (target == "") return 1;
 
         var count = 0;
-        foreach(var word in wordBank)
+        foreach (var word in wordBank)
         {
-            if(target.StartsWith(word))
+            if (target.StartsWith(word))
             {
                 var suffix = target[word.Length..];
                 count += Solve(suffix, wordBank);
             }
-        }   
+        }
         return count;
     }
 
@@ -25,9 +48,9 @@ public static class CountConstruct
         if (target == "") return 1;
 
         var count = 0;
-        foreach(var word in wordBank)
+        foreach (var word in wordBank)
         {
-            if(target.StartsWith(word))
+            if (target.StartsWith(word))
             {
                 var suffix = target[word.Length..];
                 count += Solve(suffix, wordBank, memo);
@@ -36,7 +59,7 @@ public static class CountConstruct
         memo[target] = count;
         return count;
     }
-    
+
 
     public static void Test()
     {
@@ -44,5 +67,10 @@ public static class CountConstruct
         Console.WriteLine(Solve("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef"]));
         Console.WriteLine(Solve("skateboard", ["bo", "rd", "ate", "t", "boar", "sk"]));
         Console.WriteLine(Solve("eeeeeeeeeeeeeeeeeeeeeeeeeeee", ["e", "eee", "eeeee"], []));
+        Console.WriteLine("Tab Solve");
+        Console.WriteLine(TabSolve("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+        Console.WriteLine(TabSolve("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef"]));
+        Console.WriteLine(TabSolve("skateboard", ["bo", "rd", "ate", "t", "boar", "sk"]));
+        Console.WriteLine(TabSolve("eeeeeeeeeeeeeeeeeeeeeeeeeeee", ["e", "eee", "eeeee"]));
     }
 }
